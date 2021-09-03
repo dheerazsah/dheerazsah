5f402a7f1ffaefde0b8c50f737e5d9e56eda291c
dheerazsah <83595627+dheerazsah@users.noreply.github.com>
2021-09-03 17:51:08 +0545

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -69,7 +69,27 @@ tabs.forEach(tab =>{
 })
 
 /*==================== SERVICES MODAL ====================*/
+const modalViews = document.querySelectorAll('.services__modal'),
+      modalBtns = document.querySelectorAll('.services__button'),
+      modalCloses = document.querySelectorAll('.services__modal-close')  
 
+let modal = function(modalClick){
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((modalBtn, i) => {
+    modalBtn.addEventListener('click', () =>{
+        modal(i)
+    })
+})
+
+modalCloses.forEach((modalClose) => {
+    modalClose.addEventListener('click', () =>{
+        modalViews.forEach((modalView) => {
+            modalView.classList.remove('active-modal')
+        })
+    })
+})
 
 /*==================== BLOGS SWIPER  ====================*/
 let swiper = new Swiper('.blogs__container', {
@@ -84,6 +104,7 @@ let swiper = new Swiper('.blogs__container', {
       el: ".swiper-pagination",
       clickable: true,
     },
+    
 });
 
 /*==================== TESTIMONIAL ====================*/
